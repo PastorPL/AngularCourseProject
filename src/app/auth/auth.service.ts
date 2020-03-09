e54ba6d839d99b3a602ffeb6822b1355a7429081
Michal <michal.wieckowskii@gmail.com>
2020-03-09 21:45:09 +0100

    WIP

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  signup(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDO2I7F9NWxZ_SpG06fvcpRvQit4BBwoaY',
+      {
+        email,
+        password,
+        returnSecureToken: true
+      }
+    );
+  }
 }
