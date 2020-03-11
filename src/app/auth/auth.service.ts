5b375ec30d202c8223f9ddad34b17b4e2127afdc
Michal <michal.wieckowskii@gmail.com>
2020-03-11 18:48:43 +0100

    Storing user for auth

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,21 +1,24 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {catchError} from 'rxjs/operators';
-import {throwError} from 'rxjs';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {catchError, tap} from 'rxjs/operators';
+import {Subject, throwError} from 'rxjs';
+import {User} from './user.model';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
-  registered: string;
+  registered?: string;
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  user = new Subject<User>();
 
   constructor(private http: HttpClient) { }
 
@@ -27,25 +30,61 @@ export class AuthService {
         password,
         returnSecureToken: true
       }
-    ).pipe(catchError(errorRes => {
-        let errorMessage = 'An unknown error occured';
-        if (!errorRes.error || !errorRes.error.error) {
-          return throwError(errorMessage);
-        }
-        switch (errorRes.error.error.message) {
-          case 'EMAIL_EXISTS':
-            errorMessage = 'The email address is already in use by another account.';
-            break;
-          case 'OPERATION_NOT_ALLOWED':
-            errorMessage = 'Password sign-in is disabled for this project.';
-            break;
-          case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-            errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
-            break;
-        }
-        return throwError(errorMessage);
-      })
-    );
+    ).pipe(
+      catchError(this.handleError),
+      tap(resData => {
+        this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+    }));
 
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDO2I7F9NWxZ_SpG06fvcpRvQit4BBwoaY',
+      {
+        email,
+        password,
+        returnSecureToken: true
+      })
+      .pipe(
+        catchError(this.handleError),
+        tap(resData => {
+          this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+          }
+        )
+      );
+  }
+
+  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+    const expiryDate = new Date( new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userId, token, expiryDate);
+    this.user.next(user);
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occured';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'The email address is already in use by another account.';
+        break;
+      case 'OPERATION_NOT_ALLOWED':
+        errorMessage = 'Password sign-in is disabled for this project.';
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'There is no user record corresponding to this identifier. The user may have been deleted.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'The password is invalid or the user does not have a password.';
+        break;
+      case 'USER_DISABLED':
+        errorMessage = 'The user account has been disabled by an administrator.';
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
